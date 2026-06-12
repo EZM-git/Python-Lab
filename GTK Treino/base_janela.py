6dd8279187a36fa706635fc0ba6c46fd91e3b9c6
Nathan <nathanmp201@gmail.com>
2026-06-12 14:09:16 -0300

    Update base_janela.py

--- GTK Treino/base_janela.py
+++ GTK Treino/base_janela.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: ut-8 -*-
 
+import gi
+gi.require_version("Gtk", "3.0")
+from gi.repository import Gtk 
+
+class Tela:
+    def __init__(self):
+        janela = Gtk.window()
+        janela.connect("delete-event", self.sair)
+        janela.set_title("Título da janela")
+        janela.set_border_width(20)
+        janela.set_default_size(300, 200)
+
+        janela.show_all()
